899603cea21e31d4e27735b14c7d4657f0ea9e3c
nicolas611@gmx.de <Adam Kois@DESKTOP-288UAAD.speedport.ip>
2020-10-05 20:33:26 +0200

    Added animated Coal Generator GUI

---
+++ src/main/java/net/mcreator/gratestinventions/procedures/BatteryDisplayOverlayIngameProcedure.java
@@ -0,0 +1,16 @@
+package net.mcreator.gratestinventions.procedures;
+
+import net.mcreator.gratestinventions.GratestInventionsModElements;
+
+import java.util.Map;
+
+@GratestInventionsModElements.ModElement.Tag
+public class BatteryDisplayOverlayIngameProcedure extends GratestInventionsModElements.ModElement {
+	public BatteryDisplayOverlayIngameProcedure(GratestInventionsModElements instance) {
+		super(instance, 70);
+	}
+
+	public static boolean executeProcedure(Map<String, Object> dependencies) {
+		return (false);
+	}
+}
